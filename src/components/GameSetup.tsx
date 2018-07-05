da8ea8110fb8fef14889bdf08bbb788f58e40575
Rick Kessler <rkessler@microsoft.com>
2018-07-05 09:09:01 -0700

    initial form development

--- src/components/GameSetup.tsx
+++ src/components/GameSetup.tsx
@@ -1,4 +1,9 @@
 import * as React from 'react';
+import {Button} from "react-bootstrap";
+import {ControlLabel} from "react-bootstrap";
+import {FormGroup} from "react-bootstrap";
+import {FormControl} from "react-bootstrap";
+import {HelpBlock} from "react-bootstrap";
 
 export interface GameSetupProps {}
 export class GameSetup extends React.Component<GameSetupProps, {}> {
@@ -7,7 +12,48 @@ export class GameSetup extends React.Component<GameSetupProps, {}> {
         super(props);
     }
 
+    private getValidationState(): any {
+        return null;
+    }
+
     render() {
-        return <div>Goodbye World</div>
+        return( 
+            <div className="gameSetup">
+                <form>
+                        <div className="gameSetupForm">
+                            
+                            <div className="gameSetupSection">
+                                <div className="gameSetupSectionHeader">Who's playing (and in what order)?</div>
+                                
+                                <FormGroup controlId="nameLabel">
+                                    <ControlLabel>Name</ControlLabel>
+                                    <FormControl
+                                        type="text"
+                                        placeholder="Player Name"
+                                    />
+                                    <FormControl.Feedback />
+                                    <HelpBlock>Validation is based on string length.</HelpBlock>
+                                </FormGroup>
+                            
+                            </div>
+
+                            <div className="gameSetupSection">
+                                <div className="gameSetupSectionHeader">What cards do you have?</div>
+                               
+
+                                    
+
+
+
+                            </div>
+                            <div className="gameSetupSection">
+                                <Button type="submit">Submit</Button>
+                            </div>
+
+                        </div>
+
+                   
+                </form>
+            </div>)
     }
 }
